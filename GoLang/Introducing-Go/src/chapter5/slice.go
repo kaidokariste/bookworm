1298dbadbc06fea968364256631811feb8717c41
Kaido Kariste <kaido98@hotmail.com>
2017-06-08 22:01:11 +0300

    Finishing Chapter 5

--- GoLang/Introducing-Go/src/chapter5/slice.go
+++ GoLang/Introducing-Go/src/chapter5/slice.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Here comes the slice ..")
+	sliceappend()
+	copyslice()
+}
+
+func sliceappend(){
+	slice1 := []int{1,2,3}
+	slice2 := append(slice1, 4, 5)
+	fmt.Println(slice1, slice2)
+}
+
+func copyslice() {
+	slice1 := []int{1,2,3}
+	slice2 := make([]int,2)
+	// copy (destination, source)
+	copy(slice2,slice1)
+	fmt.Println(slice2, slice1)
 }
