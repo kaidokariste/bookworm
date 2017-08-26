02aed66dcb1c9c830c9b696597a06fa16d293424
Kaido Kariste <kaido98@hotmail.com>
2017-08-26 13:49:21 +0300

    communalAPI

---
+++ GoLang/src/communalAPI/app.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"gopkg.in/mgo.v2/bson"
+
+	"github.com/gorilla/mux"
+	. "movistoreAPI/config"
+	. "movistoreAPI/dao"
+	. "movistoreAPI/models"
+
+)
+
+var config = Config{}
+var dao = MoviesDAO{}
+
+// GET list of movies
+func AllPaymentsEndPoint(w http.ResponseWriter, r *http.Request) {
+	movies, err := dao.FindAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	respondWithJson(w, http.StatusOK, movies)
+}
+
+// // GET a movie by its ID
+func FindPaymentEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	movie, err := dao.FindById(params["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Movie ID")
+		return
+	}
+	respondWithJson(w, http.StatusOK, movie)
+}
+
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJson(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}
+
+// Parse the configuration file 'config.toml', and establish a connection to DB
+func init() {
+	config.Read()
+
+	dao.Server = config.Server
+	dao.Database = config.Database
+	dao.Connect()
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/payments", AllPaymentsEndPoint).Methods("GET")
+	/*r.HandleFunc("/movies", CreateMovieEndPoint).Methods("POST")
+	r.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
+	r.HandleFunc("/movies", DeleteMovieEndPoint).Methods("DELETE")*/
+	r.HandleFunc("/payment/{id}", FindPaymentEndpoint).Methods("GET")
+	if err := http.ListenAndServe(":3000", r); err != nil {
+		log.Fatal(err)
+	}
+}
