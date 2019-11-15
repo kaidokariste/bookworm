889e13e6e187f07812071312f95d5b01382aeaa5
Kaido Kariste <kaidokariste@users.noreply.github.com>
2019-11-15 11:17:47 +0200

    Update removing-data.js

--- MongoDB-basics/removing-data.js
+++ MongoDB-basics/removing-data.js
@@ -2,3 +2,6 @@
 
 //Remove all documents where first name is larger than 30 symbols
 db['Collection'].remove( { $where: "this.FirstName.length > 30" } )
+
+// Remove all documents where exists property "myName" : "Kaido"
+db.getCollection('Collection').remove( { "myName" : "Kaido" } )
