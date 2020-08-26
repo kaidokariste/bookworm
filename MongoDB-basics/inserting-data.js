ca865dca007fbf8199e58c862517c6be83cf7f2e
Kaido Kariste <kaidokariste@users.noreply.github.com>
2020-08-26 18:42:34 +0300

    Update inserting-data.js

--- MongoDB-basics/inserting-data.js
+++ MongoDB-basics/inserting-data.js
@@ -6,3 +6,8 @@ db.cities.insert(
 		
 	]
 )
+
+// Insert some subset from Parent-Collection to Child-Collection (WHERE sentTimestamp > 1598446301802)
+db.getCollection('Parent-Collection').find({"sentTimestamp":{$gt: 1598446301802}}).forEach(function(doc){
+   db.getCollection('Child-Collection').insert(doc);
+});
